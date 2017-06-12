ffec171f1e35dabc2650f41828aa2e93e5fde4ed
Lena Lee <lenalee@Lenaui-MacBook-Pro.local>
2017-06-12 18:19:38 +0900

    1page

---
+++ My_Project/RequiredForAnyPath.swift
@@ -0,0 +1,27 @@
+//
+//  RequiredForAnyPath.swift
+//  My_Project
+//
+//  Created by Lena Lee on 2017. 6. 12..
+//  Copyright © 2017년 tk's macbook. All rights reserved.
+//
+/*Git - version control
+ -> https://ko.wikipedia.org/wiki/깃_(소프트웨어)
+ 
+ SSH
+ -> https://ko.wikipedia.org/wiki/시큐어_셸
+ 
+ Http/Https
+ -> https://ko.wikipedia.org/wiki/HTTPS
+ 
+ Basic terminal
+ -> https://ko.wikipedia.org/wiki/Basic_terminal
+ 
+ Learn to Research
+ 
+ Datastructures & Algorithms
+ 
+ 
+ Character Encodings  */
+
+import Foundation
